1a79d96a83657d39f8872de180bf3c134e4e0836
Mikołaj Lubiak <lubiakmikolaj@gmail.com>
2021-02-22 13:26:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+from keep_alive import keep_alive
+
+
+token = 'ODEyNzk5MTk2MTg4OTAxNDg4.YDGAPQ.csoHCo0FZu_eD61JnkOZaine3kA'
+
+client = discord.Client()
+client = commands.Bot(command_prefix='<KS/>')
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Kocham Skrypt"))
+    print('Bot aktywny')
+
+
+@client.event
+async def on_message(message):
+    empty_array = []
+    modmail_channel = discord.utils.get(client.get_all_channels(), name="dm-staff")
+
+    if message.author == client.user:
+        return
+    if str(message.channel.type) == "private":
+        if message.attachments != empty_array:
+            files = message.attachments
+            await modmail_channel.send("[" + message.author.display_name + "]")
+
+            for file in files:
+                await modmail_channel.send(file.url)
+        else:
+            await modmail_channel.send("[" + message.author.display_name + "] " + message.content)
+
+    elif str(message.channel) == "dm-staff" and message.content.startswith("<"):
+        member_object = message.mentions[0]
+        if message.attachments != empty_array:
+            files = message.attachments
+            await member_object.send("[" + message.author.display_name + "]")
+
+            for file in files:
+                await member_object.send(file.url)
+        else:
+            index = message.content.index(" ")
+            string = message.content
+            mod_message = string[index:]
+            await member_object.send("[" + message.author.display_name + "]" + mod_message)
+
+keep_alive()
+
+client.run(token)
